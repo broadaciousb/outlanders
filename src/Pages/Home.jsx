c1f47ce3db69f9974cde1a548337d15ae470b14a
Broady <broadybmx@gmail.com>
2025-03-21 21:16:25 -0400

    Feat: Revised new character details, started new character stats

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,33 +5,39 @@ import OutlandersMap from "../assets/OutlandersMap.JPEG";
 
 const Home = ({ register, login, logout, user }) => {
   return (
-    <div className="section">
-      <div className="landing">
-        <div className="landing__left">
-          <div className="landing__background"></div>
-        </div>
-        <div className="landing__right">
-          <div className="landing__sub-title">Welcome To</div>
-          <div className="landing__title">Outlanders</div>
-          <div className="landing__sub-title">
-            {user.email}
-          </div>
-          <ul className="landing__links">
-            <li>
-              <button className="landing__link" onClick={register}>Create Account</button>
-            </li>
-            <li>
-              <button className="landing__link" onClick={login}>Login</button>
-              <button className="landing__link" onClick={logout}>Logout</button>
-            </li>
-            <li>
-              <Link to="/characters" className="landing__link">Characters</Link>
-            </li>
-            <li>
-              <Link to="/guide" className="landing__link">Guide</Link>
-            </li>
-          </ul>
-        </div>
+    <div className="landing">
+      <div className="landing__left">
+        <div className="landing__background"></div>
+      </div>
+      <div className="landing__right">
+        <div className="landing__sub-title">Welcome To</div>
+        <div className="landing__title">Outlanders</div>
+        <div className="landing__sub-title">{user.email}</div>
+        <ul className="landing__links">
+          <li>
+            <button className="landing__link" onClick={register}>
+              Create Account
+            </button>
+          </li>
+          <li>
+            <button className="landing__link" onClick={login}>
+              Login
+            </button>
+            <button className="landing__link" onClick={logout}>
+              Logout
+            </button>
+          </li>
+          <li>
+            <Link to="/characters" className="landing__link">
+              Characters
+            </Link>
+          </li>
+          <li>
+            <Link to="/guide" className="landing__link">
+              Guide
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/Pages/NewCharacter.jsx
+++ src/Pages/NewCharacter.jsx
@@ -7,54 +7,94 @@ const NewCharacter = () => {
       <section className="new__character">
         <div className="new__character__info--wrapper">
           <div className="new__character__info">
-            <input
-              type="text"
-              className="character__info--detail new__name"
-              placeholder="Name"
-            />
-            <input
-              type="text"
-              className="character__info--detail new__class"
-              placeholder="Class"
-            />
-            <input
-              type="text"
-              className="character__info--detail new__race"
-              placeholder="Race"
-            />
-            <input
-              type="text"
-              className="character__info--detail new__diety"
-              placeholder="Diety"
-            />
-            <input
-              type="text"
-              className="character__info--detail new__homeland"
-              placeholder="Homeland"
-            />
-            <input
-              type="text"
-              className="character__info--detail new__background"
-              placeholder="Background"
-            />
-            <input
-              type="text"
-              className="character__info--detail new__size"
-              placeholder="Size"
-            />
-            <input
-              type="text"
-              className="character__info--detail new__age"
-              placeholder="Age"
-            />
-            <input
-              type="text"
-              className="character__info--detail new__sex"
-              placeholder="Sex"
-            />
+            <div className="info__detail">
+              <label htmlFor="name">Name:</label>
+              <input id="name" type="text" className="info__input" />
+            </div>
+            <div className="info__detail">
+              <label htmlFor="class">Class:</label>
+              <input id="class" type="text" className="info__input" />
+            </div>
+            <div className="info__detail">
+              <label htmlFor="Race">Race:</label>
+              <input id="Race" type="text" className="info__input" />
+            </div>
+            <div className="info__detail">
+              <label htmlFor="diety">Diety:</label>
+              <input id="diety" type="text" className="info__input" />
+            </div>
+            <div className="info__detail">
+              <label htmlFor="homeland">Homeland:</label>
+              <input id="homeland" type="text" className="info__input" />
+            </div>
+            <div className="info__detail">
+              <label htmlFor="background">Background:</label>
+              <input id="background" type="text" className="info__input" />
+            </div>
+            <div className="info__detail">
+              <label htmlFor="size">Size:</label>
+              <input id="size" type="text" className="info__input" />
+            </div>
+            <div className="info__detail">
+              <label htmlFor="age">Age:</label>
+              <input id="age" type="text" className="info__input" />
+            </div>
+            <div className="info__detail">
+              <label htmlFor="sex">Sex:</label>
+              <input id="sex" type="text" className="info__input" />
+            </div>
           </div>
           <div className="new__character__img--wrapper">
             <img className="character__img" src={character__img} alt="" />
+          </div>
+        </div>
+        <div className="character__stats--wrapper">
+          <div className="character__stats">
+            <div className="stat__column stat__names">
+              <div className="stat__name">STAT</div>
+              <div className="stat__name">STR</div>
+              <div className="stat__name">DEX</div>
+              <div className="stat__name">CON</div>
+              <div className="stat__name">INT</div>
+              <div className="stat__name">WIS</div>
+              <div className="stat__name">CHA</div>
+            </div>
+            <div className="stat__column stat__values">
+              <div className="stat__value">SCORE</div>
+              <div className="stat__value">6</div>
+              <div className="stat__value">5</div>
+              <div className="stat__value">5</div>
+              <div className="stat__value">4</div>
+              <div className="stat__value">3</div>
+              <div className="stat__value">3</div>
+            </div>
+            <div className="stat__column stat__values">
+              <div className="stat__value">MOD</div>
+              <div className="stat__value">6</div>
+              <div className="stat__value">5</div>
+              <div className="stat__value">5</div>
+              <div className="stat__value">4</div>
+              <div className="stat__value">3</div>
+              <div className="stat__value">3</div>
+            </div>
+            <div className="stat__column stat__values">
+              <div className="stat__value">BONUS</div>
+              <div className="stat__value">6</div>
+              <div className="stat__value">5</div>
+              <div className="stat__value">5</div>
+              <div className="stat__value">4</div>
+              <div className="stat__value">3</div>
+              <div className="stat__value">3</div>
+            </div>
+            <div className="stat__column stat__values">
+              <div className="stat__value">BASE</div>
+              <div className="stat__value">6</div>
+              <div className="stat__value">5</div>
+              <div className="stat__value">5</div>
+              <div className="stat__value">4</div>
+              <div className="stat__value">3</div>
+              <div className="stat__value">3</div>
+            </div>
           </div>
         </div>
       </section>
